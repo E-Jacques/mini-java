940114729f68a319e83db6be0e08221bbfc27b63
Emilien <emil00j77@gmail.com>
2021-12-17 15:34:14 +0100

    Handling index out of range

--- src/codegen/ToMipsPlus.java
+++ src/codegen/ToMipsPlus.java
@@ -24,47 +24,88 @@ public class ToMipsPlus extends ToMips {
 
     // @Override
     // public void visit(final QJumpCond q) {
-    //     Reg r = this.tmpRegLoad(q.arg2, Reg.V0);
-    //     mw.jumpIfNot(r, q.arg1.getName());
+    // Reg r = this.tmpRegLoad(q.arg2, Reg.V0);
+    // mw.jumpIfNot(r, q.arg1.getName());
     // }
 
     // @Override
     // public void visit(final QAssignUnary q) {
-    //     Reg r = this.tmpRegLoad(q.arg1, Reg.V0);
-    //     if (q.op == main.EnumOper.NOT) {
-    //         mw.not(r);
-    //     }
+    // Reg r = this.tmpRegLoad(q.arg1, Reg.V0);
+    // if (q.op == main.EnumOper.NOT) {
+    // mw.not(r);
+    // }
 
-    //     this.regStore(Reg.V0, q.result);
+    // this.regStore(Reg.V0, q.result);
     // }
 
     // @Override
     // public void visit(QAssign q) {
-    //     Reg r0 = tmpRegLoad(q.arg1, Reg.V0);
-    //     Reg r1 = tmpRegLoad(q.arg2, Reg.V1);
+    // Reg r0 = tmpRegLoad(q.arg1, Reg.V0);
+    // Reg r1 = tmpRegLoad(q.arg2, Reg.V1);
 
-    //     switch (q.op) {
-    //         case PLUS:
-    //             mw.plus(r0, r1);
-    //             break;
-    //         case TIMES:
-    //             mw.fois(r0, r1);
-    //             break;
-    //         case MINUS:
-    //             mw.moins(r0, r1);
-    //             break;
-    //         case AND:
-    //             mw.et(r0, r1);
-    //             break;
-    //         case LESS:
-    //             mw.inferieur(r0, r1);
-    //             break;
-    //         default:
-    //             break;
-    //     }
-
-    //     this.regStore(r0, q.result);
+    // switch (q.op) {
+    // case PLUS:
+    // mw.plus(r0, r1);
+    // break;
+    // case TIMES:
+    // mw.fois(r0, r1);
+    // break;
+    // case MINUS:
+    // mw.moins(r0, r1);
+    // break;
+    // case AND:
+    // mw.et(r0, r1);
+    // break;
+    // case LESS:
+    // mw.inferieur(r0, r1);
+    // break;
+    // default:
+    // break;
     // }
+
+    // this.regStore(r0, q.result);
+    // }
+
+    @Override
+    public void visit(final QAssignArrayFrom q) {
+        push(Reg.T0, Reg.T1, Reg.T2, Reg.T3);
+
+        this.regLoad(Reg.T0, q.arg1);
+        this.regLoad(Reg.T1, q.arg2);
+
+        mw.loadOffset(Reg.T2, 0, Reg.T0);
+        mw.inRange(Reg.T3, Reg.T1, Reg.T2);
+        mw.jumpIfNot(Reg.T3, "_system_exit");
+
+        mw.fois4(Reg.T1);
+        mw.plus(Reg.T0, Reg.T1);
+
+        mw.loadOffset(Reg.V0, 4, Reg.T0);
+        this.regStore(Reg.V0, q.result);
+
+        pop(Reg.T0, Reg.T1, Reg.T2, Reg.T3);
+    }
+
+    @Override
+    public void visit(final QAssignArrayTo q) {
+        push(Reg.T0, Reg.T1, Reg.T2, Reg.T3, Reg.A0);
+
+        this.regLoad(Reg.T0, q.result);
+        this.regLoad(Reg.T1, q.arg2);
+
+        mw.loadOffset(Reg.T2, 0, Reg.T0);
+        mw.inRange(Reg.T3, Reg.T1, Reg.T2);
+        mw.load(Reg.A0, 10);
+        mw.jumpIfNot(Reg.T3, "_system_exit");
+
+        mw.fois4(Reg.T1);
+        mw.plus(Reg.T0, Reg.T1);
+
+        this.regLoad(Reg.V0, q.arg1);
+        mw.storeOffset(Reg.V0, 4, Reg.T0);
+
+        pop(Reg.T0, Reg.T1, Reg.T2, Reg.T3, Reg.A0);
+    }
 
     @Override
     public void visit(final QCall q) throws CompilerException {
